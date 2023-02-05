43f6b62fecd1c7cff908a07c2f5e72762d4e2684
Your Name <you@example.com>
2023-02-05 15:39:08 +0300

    added somtihng

--- src/Labs.Lib/StringExtentnsions.cs
+++ src/Labs.Lib/StringExtentnsions.cs
@@ -3,4 +3,17 @@
 public static class StringExtentions
 {
 
+    public static string[] GetAllBorders(this string value)
+    {
+        var res = new List<string>();
+        for (int i = 1; i < value.Length; i++) {
+
+            if (value.Substring(0, i) == value.Substring(value.Length - i, i)) {
+                res.Add(value.Substring(0,i));
+            }
+        }
+
+        res.Sort();
+        return res.ToArray();
+    }
 }
--- tests/Labs.Lib.Tests/StringExptensionsTests.cs
+++ tests/Labs.Lib.Tests/StringExptensionsTests.cs
@@ -8,7 +8,11 @@ public class StringExtenstionsTests
     [MemberData(nameof(BordersTestsData))]
     public void Returns_Border_For_String(string value, string[] excepted)
     {
+        //act
+        var result = value.GetAllBorders();
 
+        //assert 
+        result.Should().BeEquivalentTo(excepted);
     }
 
     [Theory]
@@ -17,14 +21,6 @@ public class StringExtenstionsTests
     {
 
     }
-
-    [Theory]
-    [MemberData(nameof(PrefixesTestsData))]
-    public void Returns_Array_Of_Prefixes_For_String(string value, string[] excepted)
-    {
-
-    }
-        
     #endregion
 
 
@@ -33,18 +29,29 @@ public class StringExtenstionsTests
         
     public static IEnumerable<object[]> SufixesTestsData = new List<object[]> ()
     {
-        new object[] {"ab", new string[] {}},
-        new object[] {"abba", new string[] {}},
-    };
-
-    public static IEnumerable<object[]> PrefixesTestsData = new List<object[]> ()
-    {
-        new object[] {"ab", new string[] {}},
-        new object[] {"abba", new string[] {}},
+        new object[] {},
     };
 
     public static IEnumerable<object[]> BordersTestsData = new List<object[]> ()
     {
+        new object[] {"ab", new string[] {}},
+        new object[] {"abba", new string[] {"a"}},
+        new object[] {"abbabaab", new string[] {"ab"}},
+        new object[] {"abbabaabbaababba", new string[] {"a", "abba"}},
+        new object[] {"abbabaabbaababbabaababbaabbabaab", new string[] {"ab", "abbabaab"}},
+        new object[] {"abcab", new string[] {"ab"}},
+        new object[] {"abcabacabcbacbcacbabcabacabcb", new string[] {"abcabacabcb"}},
+        new object[]
+        {
+            "abcabacabcbacbcacbabcabacabcbabcabacbcacbabcabacabcbacbcacbacabcbabcabacbcacbacabcbacbcacbabcabacbcacbacabcbabcabacabcbacbcacbabcabacabcbabcabacbcacbabcabacabcbacbcacbacabcb",
+            new string[] {"abcabacabcbacbcacbabcabacabcbabcabacbcacbabcabacabcbacbcacbacabcb"}
+        },
+        new object[] {"aba", new string[] {"a"}},
+        new object[] {"abaab", new string[] {"ab"}},
+        new object[] {"abaabaab", new string[] {"ab", "abaab"}},
+        new object[] {"abaababaabaab", new string[] {"ab", "abaab"}},
+        new object[] {"abaababaabaababaabaab", new string[] {"ab", "abaab", "abaababaabaab"}},
+        new object[] {"abaababaabaababaabaababaababaabaab", new string[] {"ab", "abaab", "abaababaabaab"}},
     };
     #endregion
 }
