6c9bdac70343bf7be832a7ec13490ba9cfa39ad2
Your Name <you@example.com>
2023-02-05 17:28:03 +0300

    added implementation of GetSubstring

--- src/Labs.Lib/StringExtentnsions.cs
+++ src/Labs.Lib/StringExtentnsions.cs
@@ -1,4 +1,6 @@
-﻿namespace Labs.Lib;
+﻿using System.Runtime.CompilerServices;
+
+namespace Labs.Lib;
 
 public static class StringExtentions
 {
@@ -16,4 +18,52 @@ public static class StringExtentions
         res.Sort();
         return res.ToArray();
     }
+
+    public static IEnumerable<int> GetSubstring(this string pattern, string text) => 
+        new BorderString().SearchSubstring(pattern, text);
+    
+    private class BorderString 
+    {
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private String JoinRows(String s1, String s2) => s1 + "$" + s2;
+
+        private IEnumerable<int> MaxBorderArray(String str) 
+        {
+            int n = str.Length;
+            var borders = new int[n];
+            borders[0] = -1;
+
+            for (int i = 0; i < n - 1; i++) {
+                int tmp = borders[i];
+
+                while ((tmp > -1) && (str[i + 1] != str[tmp + 1])) tmp = borders[tmp];
+
+                if (str[i + 1] == str[tmp + 1])
+                    borders[i + 1] = tmp + 1;
+                else
+                    borders[i + 1] = -1;
+            }
+
+            return borders;
+        }
+
+        public List<int> SearchSubstring(String pattern, String sample) 
+        {
+            String str = JoinRows(pattern, sample);
+
+            var borders = MaxBorderArray(str).ToArray();
+
+            int lengthPattern = pattern.Length;
+            var result = new List<int>();
+
+            for (int i = 0; i < borders.Length; i++) {
+                if ((borders[i] + 1) == pattern.Length)
+                    result.Add(i);
+            }
+
+            return result;
+        }
+    }
+
 }
