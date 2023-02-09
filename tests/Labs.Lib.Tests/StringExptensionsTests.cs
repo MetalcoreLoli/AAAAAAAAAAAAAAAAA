c4c0e3cce1d436c602690025d4f1bf6d6c9290ad
Your Name <you@example.com>
2023-02-09 18:15:23 +0300

    fixed example

--- tests/Labs.Lib.Tests/StringExptensionsTests.cs
+++ tests/Labs.Lib.Tests/StringExptensionsTests.cs
@@ -57,7 +57,7 @@ public class StringExtenstionsTests
     {               
                     // 0  1  2  3 4 5 6  7  8
                     // a  b  a  $ a b a  b  a
-                    //-1 -1 -1 -1 0 1 2 -1 -1
+                    //-1 -1 -1 -1 0 1 2  1  2
         new object[] {"aba","ababa", new int[] {6, 8}},
 
                     // 0  1  2 3 4  5 6 7
