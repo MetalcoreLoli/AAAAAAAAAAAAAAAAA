d07c2fbca0b422f5a4b352d16023754b50dacde8
Your Name <you@example.com>
2023-02-12 12:36:13 +0300

    simplified GetAllBorders

--- src/Labs.Lib/StringExtensions.cs
+++ src/Labs.Lib/StringExtensions.cs
@@ -7,10 +7,9 @@ public static partial class StringExtensions
     /// Методов поиска граней в строка <paramref name="value"/>
     ///</summary>
     ///<param name="value"> Строка, в которой ищутся грани</param>
-    public static string[] GetAllBorders(this string value)
+    ///<returns>Возвращает массив граней</returns>
+    public static IEnumerable<string> GetAllBorders(this string value)
     {
-        // список граней
-        var borders = new List<string>();
         for (int i = 1; i < value.Length; i++) 
         {
             // сравниваем префикс и суффикс
@@ -19,12 +18,9 @@ public static partial class StringExtensions
                 //так как грань - это подстрока, которая одновременно 
                 //префикс и суффикс, то без разнице что добавлять в 
                 //список. в данном случае добавляется префикс
-                borders.Add(value.Substring(0,i));
+                yield return value.Substring(0,i);
             }
         }
-
-        borders.Sort();
-        return borders.ToArray();
     }
 
     public static IEnumerable<int> GetSubstrings(this string pattern, string text, IMethodOfSearchInString methodOfSearch) => 
